78421cbf6a617a95d35a574592b4ee8148df50fb
Jake Parkhouse <jparkhouse1511@gmail.com>
2025-01-27 00:36:20 +0000

    added unconstrained attack map trait

--- src/chess_state/moves/attack_maps.rs
+++ src/chess_state/moves/attack_maps.rs
@@ -1,71 +1,73 @@
-use super::chess_move::{ChessMove, ChessMoves};
+use super::chess_move::{ChessDirection, ChessShiftMove};
 
-trait WhitePawnAttackMaps {
+pub trait WhitePawnAttackMaps {
     fn calculate_unconstrained_white_pawn_attack_maps(self) -> Self;
 }
 
-trait BlackPawnAttackMaps {
+pub trait BlackPawnAttackMaps {
     fn calculate_unconstrained_black_pawn_attack_maps(self) -> Self;
 }
 
-trait KnightAttackMaps {
+pub trait KnightAttackMaps {
     fn calculate_unconstrained_knight_maps(self) -> Self;
 }
 
-trait BishopAttackMaps {
+pub trait BishopAttackMaps {
     fn calculate_unconstrained_bishop_attack_maps(self) -> Self;
 }
 
-trait QueenAttackMaps {
+pub trait QueenAttackMaps {
     fn calculate_unconstrained_queen_attack_maps(self) -> Self;
 }
 
-trait RookAttackMaps {
+pub trait RookAttackMaps {
     fn calculate_unconstrained_rook_attack_maps(self) -> Self;
 }
 
-trait KingAttackMaps {
+pub trait KingAttackMaps {
     fn calculate_unconstrained_king_attack_maps(self) -> Self;
 }
 
 impl WhitePawnAttackMaps for u64 {
     fn calculate_unconstrained_white_pawn_attack_maps(self) -> Self {
-        self.shift_move(ChessMove::UpLeft) | self.shift_move(ChessMove::UpRight)
+        self.shift_move(ChessDirection::UpLeft) | self.shift_move(ChessDirection::UpRight)
     }
 }
 
 impl BlackPawnAttackMaps for u64 {
     fn calculate_unconstrained_black_pawn_attack_maps(self) -> Self {
-        self.shift_move(ChessMove::DownLeft) | self.shift_move(ChessMove::DownRight)
+        self.shift_move(ChessDirection::DownLeft) | self.shift_move(ChessDirection::DownRight)
     }
 }
 
 impl KnightAttackMaps for u64 {
     fn calculate_unconstrained_knight_maps(self) -> Self {
-        self.shift_move(ChessMove::KnightOne)
-            | self.shift_move(ChessMove::KnightTwo)
-            | self.shift_move(ChessMove::KnightFour)
-            | self.shift_move(ChessMove::KnightFive)
-            | self.shift_move(ChessMove::KnightSeven)
-            | self.shift_move(ChessMove::KnightEight)
-            | self.shift_move(ChessMove::KnightTen)
-            | self.shift_move(ChessMove::KnightEleven)
+        self.shift_move(ChessDirection::KnightOne)
+            | self.shift_move(ChessDirection::KnightTwo)
+            | self.shift_move(ChessDirection::KnightFour)
+            | self.shift_move(ChessDirection::KnightFive)
+            | self.shift_move(ChessDirection::KnightSeven)
+            | self.shift_move(ChessDirection::KnightEight)
+            | self.shift_move(ChessDirection::KnightTen)
+            | self.shift_move(ChessDirection::KnightEleven)
     }
 }
 
 impl BishopAttackMaps for u64 {
     fn calculate_unconstrained_bishop_attack_maps(self) -> Self {
-        let up_right: u64 = (0..7).fold(self.shift_move(ChessMove::UpRight), |current, _| {
-            current | current.shift_move(ChessMove::UpRight)
+        let up_right: u64 = (0..7).fold(self.shift_move(ChessDirection::UpRight), |current, _| {
+            current | current.shift_move(ChessDirection::UpRight)
         });
-        let down_right: u64 = (0..7).fold(self.shift_move(ChessMove::DownRight), |current, _| {
-            current | current.shift_move(ChessMove::DownRight)
-        });
-        let down_left: u64 = (0..7).fold(self.shift_move(ChessMove::DownLeft), |current, _| {
-            current | current.shift_move(ChessMove::DownLeft)
-        });
-        let up_left: u64 = (0..7).fold(self.shift_move(ChessMove::UpLeft), |current, _| {
-            current | current.shift_move(ChessMove::UpLeft)
+        let down_right: u64 = (0..7)
+            .fold(self.shift_move(ChessDirection::DownRight), |current, _| {
+                current | current.shift_move(ChessDirection::DownRight)
+            });
+        let down_left: u64 = (0..7)
+            .fold(self.shift_move(ChessDirection::DownLeft), |current, _| {
+                current | current.shift_move(ChessDirection::DownLeft)
+            });
+        let up_left: u64 = (0..7).fold(self.shift_move(ChessDirection::UpLeft), |current, _| {
+            current | current.shift_move(ChessDirection::UpLeft)
         });
         up_right | down_right | down_left | up_left
     }
@@ -73,17 +75,17 @@ impl BishopAttackMaps for u64 {
 
 impl RookAttackMaps for u64 {
     fn calculate_unconstrained_rook_attack_maps(self) -> Self {
-        let up: u64 = (0..7).fold(self.shift_move(ChessMove::Up), |current, _| {
-            current | current.shift_move(ChessMove::Up)
+        let up: u64 = (0..7).fold(self.shift_move(ChessDirection::Up), |current, _| {
+            current | current.shift_move(ChessDirection::Up)
         });
-        let right: u64 = (0..7).fold(self.shift_move(ChessMove::Right), |current, _| {
-            current | current.shift_move(ChessMove::Right)
+        let right: u64 = (0..7).fold(self.shift_move(ChessDirection::Right), |current, _| {
+            current | current.shift_move(ChessDirection::Right)
         });
-        let down: u64 = (0..7).fold(self.shift_move(ChessMove::Down), |current, _| {
-            current | current.shift_move(ChessMove::Down)
+        let down: u64 = (0..7).fold(self.shift_move(ChessDirection::Down), |current, _| {
+            current | current.shift_move(ChessDirection::Down)
         });
-        let left: u64 = (0..7).fold(self.shift_move(ChessMove::Left), |current, _| {
-            current | current.shift_move(ChessMove::Left)
+        let left: u64 = (0..7).fold(self.shift_move(ChessDirection::Left), |current, _| {
+            current | current.shift_move(ChessDirection::Left)
         });
         up | right | down | left
     }
@@ -91,29 +93,31 @@ impl RookAttackMaps for u64 {
 
 impl QueenAttackMaps for u64 {
     fn calculate_unconstrained_queen_attack_maps(self) -> Self {
-        let up: u64 = (0..7).fold(self.shift_move(ChessMove::Up), |current, _| {
-            current | current.shift_move(ChessMove::Up)
+        let up: u64 = (0..7).fold(self.shift_move(ChessDirection::Up), |current, _| {
+            current | current.shift_move(ChessDirection::Up)
         });
-        let up_right: u64 = (0..7).fold(self.shift_move(ChessMove::UpRight), |current, _| {
-            current | current.shift_move(ChessMove::UpRight)
+        let up_right: u64 = (0..7).fold(self.shift_move(ChessDirection::UpRight), |current, _| {
+            current | current.shift_move(ChessDirection::UpRight)
         });
-        let right: u64 = (0..7).fold(self.shift_move(ChessMove::Right), |current, _| {
-            current | current.shift_move(ChessMove::Right)
+        let right: u64 = (0..7).fold(self.shift_move(ChessDirection::Right), |current, _| {
+            current | current.shift_move(ChessDirection::Right)
         });
-        let down_right: u64 = (0..7).fold(self.shift_move(ChessMove::DownRight), |current, _| {
-            current | current.shift_move(ChessMove::DownRight)
+        let down_right: u64 = (0..7)
+            .fold(self.shift_move(ChessDirection::DownRight), |current, _| {
+                current | current.shift_move(ChessDirection::DownRight)
+            });
+        let down: u64 = (0..7).fold(self.shift_move(ChessDirection::Down), |current, _| {
+            current | current.shift_move(ChessDirection::Down)
         });
-        let down: u64 = (0..7).fold(self.shift_move(ChessMove::Down), |current, _| {
-            current | current.shift_move(ChessMove::Down)
+        let down_left: u64 = (0..7)
+            .fold(self.shift_move(ChessDirection::DownLeft), |current, _| {
+                current | current.shift_move(ChessDirection::DownLeft)
+            });
+        let left: u64 = (0..7).fold(self.shift_move(ChessDirection::Left), |current, _| {
+            current | current.shift_move(ChessDirection::Left)
         });
-        let down_left: u64 = (0..7).fold(self.shift_move(ChessMove::DownLeft), |current, _| {
-            current | current.shift_move(ChessMove::DownLeft)
-        });
-        let left: u64 = (0..7).fold(self.shift_move(ChessMove::Left), |current, _| {
-            current | current.shift_move(ChessMove::Left)
-        });
-        let up_left: u64 = (0..7).fold(self.shift_move(ChessMove::UpLeft), |current, _| {
-            current | current.shift_move(ChessMove::UpLeft)
+        let up_left: u64 = (0..7).fold(self.shift_move(ChessDirection::UpLeft), |current, _| {
+            current | current.shift_move(ChessDirection::UpLeft)
         });
         up | up_right | right | down_right | down | down_left | left | up_left
     }
@@ -121,14 +125,14 @@ impl QueenAttackMaps for u64 {
 
 impl KingAttackMaps for u64 {
     fn calculate_unconstrained_king_attack_maps(self) -> Self {
-        self.shift_move(ChessMove::Up)
-            | self.shift_move(ChessMove::UpRight)
-            | self.shift_move(ChessMove::Right)
-            | self.shift_move(ChessMove::DownRight)
-            | self.shift_move(ChessMove::Down)
-            | self.shift_move(ChessMove::DownLeft)
-            | self.shift_move(ChessMove::Left)
-            | self.shift_move(ChessMove::UpLeft)
+        self.shift_move(ChessDirection::Up)
+            | self.shift_move(ChessDirection::UpRight)
+            | self.shift_move(ChessDirection::Right)
+            | self.shift_move(ChessDirection::DownRight)
+            | self.shift_move(ChessDirection::Down)
+            | self.shift_move(ChessDirection::DownLeft)
+            | self.shift_move(ChessDirection::Left)
+            | self.shift_move(ChessDirection::UpLeft)
     }
 }
 
@@ -137,7 +141,9 @@ mod test {
     mod attack_maps_for_u64 {
         use crate::chess_state::{
             coordinates::{XCoordinate::*, YCoordinate::*},
-            moves::attack_maps::{BishopAttackMaps, BlackPawnAttackMaps, KnightAttackMaps, WhitePawnAttackMaps},
+            moves::attack_maps::{
+                BishopAttackMaps, BlackPawnAttackMaps, KnightAttackMaps, WhitePawnAttackMaps,
+            },
         };
 
         #[test]
@@ -178,13 +184,12 @@ mod test {
                 | ((A as u64) & (Two as u64))
                 | ((A as u64) & (Four as u64))
                 | ((B as u64) & (Five as u64));
-        
+
             // act
             let attack_map = knight.calculate_unconstrained_knight_maps();
-        
+
             // assert
             assert_eq!(attack_map, expected_attack_map)
-        
         }
 
         #[test]

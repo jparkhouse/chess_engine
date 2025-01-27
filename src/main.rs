7b4410ee2cf8245446ac2eff5bde8b8c23f267e6
Jake Parkhouse <jparkhouse1511@gmail.com>
2025-01-27 00:35:17 +0000

    added logging infra

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-
-use chess_state::chess_pieces::{PieceEnum, piece_structs::*};
+use chess_state::chess_pieces::{piece_structs::*, PieceEnum};
+use env_logger::Builder;
+use std::io::Write;
 
 mod bitmask;
 mod chess_state;
@@ -7,5 +8,19 @@ mod evaluation_engine;
 mod shared;
 
 fn main() {
+    let mut builder = Builder::from_default_env();
+    builder
+        .format(|buf, record| {
+            writeln!(
+                buf,
+                "{} [{}] - {}",
+                chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
+                record.level(),
+                record.args()
+            )
+        })
+        .init();
+    log::info!("Initialised logger");
+
     println!("Hello, world!");
 }
